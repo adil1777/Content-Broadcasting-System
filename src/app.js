6c6fdde6924ebe808232619bef01e05aa47d3428
adil2024 <163632468+adil2024@users.noreply.github.com>
2026-04-26 10:49:33 +0530

    Initial project setup

---
+++ src/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const cors = require("cors");
+const morgan= require('morgan');
+const connectDB = require("./config/dbConnection");
+
+dotenv.config();
+const app = express();
+app.use(cors());
+
+//Mongodb Connection
+connectDB();
+
+//middleware 
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+
+
+// Routes for testing
+app.get("/", async (req, res) => {
+  try {
+    res.send("Hello Word");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+
+//Routes
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on Port ${PORT}`.bgGreen.white);
+});
